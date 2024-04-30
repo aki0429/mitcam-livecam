0055893e3a6aaafb36103bdd929bde436f19fb5e
アッキー.net <96721364+aki0429@users.noreply.github.com>
2024-04-30 17:49:28 +0900

    kitasyuto　追加(国道）

---
+++ load/kitasyuto・utunomiya/constants/cams.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cams = void 0;
+exports.cams = ["C01770","C01769","C01754","C01755","C05004","C05005","C01756","C01757","C01758","C01759","C05006","C05007","C05008","C05009","C01760","C01761","C01761","C01762","C01763","C01764","C05012","C05013","C01765","C01766","C01767","C01768"];
+//# sourceMappingURL=cams.js.map
